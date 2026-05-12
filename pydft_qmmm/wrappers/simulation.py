1198bb051431f0967f805d18b4f27ca55451a826
cscott421 <153154342+cscott421@users.noreply.github.com>
2026-05-12 12:06:25 -0400

    Update simulation.py

--- pydft_qmmm/wrappers/simulation.py
+++ pydft_qmmm/wrappers/simulation.py
@@ -123,6 +123,7 @@ class Simulation(Loggable):
         temp = self.system.positions.base.copy()
         results = self.calculator.calculate()
         self.system.forces = results.forces
+        self.force_components = results.force_components  # store for access
         kinetic_energy = self.integrator.compute_kinetic_energy(
             self.system,
         )
@@ -137,6 +138,9 @@ class Simulation(Loggable):
         self.energy = energy
         self._offset += temp - self.system.positions.base
         logger.info("", extra={"frame": self._frame, "energy": self.energy})
+        for key, forces in self.force_components.items():
+            if isinstance(forces, np.ndarray):  # skip nested dicts
+                logger.info("", extra={"frame": self._frame, "forces": forces})
 
     def _register_plugins(self, plugins: list[Plugin]) -> None:
         """Dynamically load plugins for calculators and integrators.
